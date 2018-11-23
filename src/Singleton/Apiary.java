faefc57dd73524e06388edf1a38bbcd9b74b0293
DevForrest <Dev.Forrester@gmail.com>
2018-11-23 12:58:41 -0800

    Apiary class start

--- src/Singleton/Apiary.java
+++ src/Singleton/Apiary.java
@@ -1,5 +1,25 @@
 package Singleton;
 
 public class Apiary {
-
+    
+    // single accessible instance
+    private static Apiary apiaryInstance;
+    
+    // private constructor
+    private Apiary() {
+        
+    }
+    
+    // point of access
+    public static Apiary getApiary() {
+        if (apiaryInstance == null) {
+            apiaryInstance = new Apiary();
+        }
+        return apiaryInstance;
+    }
+    
+    // display method
+    public void displayApiary() {
+        System.out.println("Print apiary singleton values");
+    }
 }
