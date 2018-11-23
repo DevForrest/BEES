9160253e6b9d08563bf28574d8a4533d4c9eb9d3
DevForrest <Dev.Forrester@gmail.com>
2018-11-23 13:07:01 -0800

    Started setting up CheckStyle

--- src/Singleton/Apiary.java
+++ src/Singleton/Apiary.java
@@ -1,23 +1,26 @@
-package Singleton;
+package singleton;
 
 public class Apiary {
-    
+
     // single accessible instance
     private static Apiary apiaryInstance;
-    
+
     // private constructor
     private Apiary() {
-        
+
     }
-    
-    // point of access
+
+    /**
+     * Gets instance; point of access.
+     * @return Apiary instance
+     */
     public static Apiary getApiary() {
         if (apiaryInstance == null) {
             apiaryInstance = new Apiary();
         }
         return apiaryInstance;
     }
-    
+
     // display method
     public void displayApiary() {
         System.out.println("Print apiary singleton values");
