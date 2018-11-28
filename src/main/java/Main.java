b64a3c7032cb0f2aeb9c0d3c0245cd379c6cf480
DevForrest <Dev.Forrester@gmail.com>
2018-11-27 21:21:38 -0800

    altered gradle functionality

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,15 @@
+import decorator.BeeImpl;
 
 public class Main {
-
+    
+    /**
+     * main runs program.
+     * @param args arguments
+     */
     public static void main(String[] args) {
         System.out.println("Hello");
+        BeeImpl bee1 = new BeeImpl();
+        System.out.println(bee1.getDescription());
 
     }
 
